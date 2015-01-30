7280036e3705c14d42ba6e575037e2dca1b3ebe4
Piera <pierad@gmail.com>
2015-01-30 11:14:58 -0800

    Binary search returns index of target

--- explorations/binary_search.py
+++ explorations/binary_search.py
@@ -14,8 +14,27 @@ def binary_search(array, target_item):
 
 lst = [1,2,3,4,5,6,7,8]
 target = 7
+start = lst[0]
+end = lst[-1]
 
-binary_search(lst, target)
+# binary_search(lst, target)
+# Returns the index of the recursively shortened list. 
 
-# Returns the index of the recursively shortened list.  
 # Q: How to return the index of the original list?
+# A: Don't split the list, instead just pass start and end.
+
+
+def bsearch(lst, start, end, target):
+	pointer = (end+start)/2
+	if lst[pointer] == target:
+		print pointer
+		return pointer
+	elif lst[pointer] > target:
+		return bsearch(lst, 0, pointer, target)
+	elif lst[pointer] < target:
+		return bsearch(lst, pointer, end, target)
+
+bsearch(lst, start, end, target)
+
+
+
