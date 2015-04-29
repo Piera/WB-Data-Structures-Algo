bbaa9a08aae33e9b7df71d0c310ec7b8914c7849
Piera <pierad@gmail.com>
2015-04-29 15:06:54 -0700

    Racing through List-1

---
+++ CodingBat/make_pi.py
@@ -0,0 +1,11 @@
+# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}. 
+
+# make_pi() # [3, 1, 4]
+
+def make_pi():
+	print [3, 1, 4]
+	return [3, 1, 4]
+
+make_pi() # [3, 1, 4]
+
+# What? Too00o0o0 easy!!!
